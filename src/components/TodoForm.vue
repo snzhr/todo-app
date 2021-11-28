d83911b72deba34c7d7003b6532038510fd7df2d
snzhr <snzhrski@gmail.com>
2021-11-28 15:34:59 +0600

    app built

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="todoForm mb-5 row justify-content-center">
+  <div class="col-8">
+  <input type="email" 
+  class="form-control" 
+  id="exampleFormControlInput1" 
+  placeholder="Learn Vue.js"
+  v-model="todo.title"
+  @keyup.enter="addTodoitem">
+  </div>
+  <div class="col-2">
+  <button type="button" @click="addTodoitem" class="btn btn-primary">Add Todo</button>
+  </div>
+
+</div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      todo:{
+          title:''
+      }
+    }
+  },
+  methods:{
+    addTodoitem(){
+      this.$emit('addtodo', this.todo)
+      this.todo = {
+        title: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card mb-2" v-for="todo in todoItems" :key="todo.id">
+  <div class="card-body">
+   <h6 class="card-title">{{todo.title}}</h6>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+    props:{
+        todoItems:{
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
